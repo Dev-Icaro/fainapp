9c6eaf16f7292aa71bd9f24127b935bfe2c6c432
Icaro-Softica <icarokiilermelo@gmail.com>
2023-12-06 17:04:02 -0300

    Initial commit with setups

---
+++ backend/src/common/utils/systemConstants.ts
@@ -0,0 +1,8 @@
+export enum HttpStatus {
+  NOT_FOUND = 404,
+  OK = 200,
+  CREATED = 201,
+  NO_CONTENT = 204,
+  SERVER_ERROR = 500,
+  BAD_REQUEST = 400,
+}
---
+++ backend/src/server.ts
@@ -0,0 +1,28 @@
+import AppException from '@common/exceptions/AppException';
+import { HttpStatus } from '@common/utils/systemConstants';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import 'dotenv/config';
+
+const app = express();
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: Error, request: Request, response: Response, _next: NextFunction) => {
+  if (error instanceof AppException) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  } else {
+    return response.status(HttpStatus.SERVER_ERROR).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  }
+});
+
+const port = process.env.APP_PORT || 3333;
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server started on port ${port}`);
+});
